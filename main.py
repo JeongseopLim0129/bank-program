740cf71f640ac6051ddc256489ae028517981ce6
seop <99js369@naver.com>
2025-09-26 12:33:52 +0900

    예외처리 추가

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 class BankAccount:
     def open_account():
         print('\n[계좌개설]')
-        a = int(input('계좌ID:(숫자로 입력)'))
-        accounts[a] = [input('이름: '), int(input('입금액: '))]
+        try:
+            a = int(input('계좌ID:(숫자로 입력)'))
+        except ValueError:
+            print('숫자만 입력하세요')
+        try:
+            amount = int(input('입금액: '))
+        except:
+            print('숫자만 입력하세요')
+        accounts[a][0] = [input('이름: ')]
+        accounts[a][1] = amount
 
     def deposit():
         for i in accounts.keys():
@@ -29,33 +37,51 @@ accounts = {}
 i = 0
 while i != 5:
     print('-----MENU-----\n1. 계좌개설\n2. 입금\n3. 출금\n4. 계좌번호 전체 출력\n5. 프로그램 종료')
-
-    i = int(input('선택(1~5 숫자만 입력) : '))
+    try:
+        i = int(input('선택(1~5 숫자만 입력) : '))
+    except ValueError:
+        print('1~5의 숫자만 입력하세요')
 
     if i == 1:
         BankAccount.open_account()
 
     elif i == 2:
         print('\n[입금]')
-        deposit_id = int(input('계좌ID: '))
-        deposit_amount = int(input('입금액: '))
+        try:
+            deposit_id = int(input('계좌ID: '))
+        except:
+            print('숫자만 입력하세요')
+        try:
+            deposit_amount = int(input('입금액: '))
+        except:
+            print('숫자만 입력하세요')
 
         if deposit_id in accounts.keys():
             BankAccount.deposit
         else:
             print('유효하지 않은 ID 입니다')
             break
+            # return
+            # sys.exit()
 
     elif i == 3:
         print('\n[출금]')
-        withdrawal_id = int(input('계좌ID: '))
-        withdrawal_amount = int(input('출금액: '))
+        try:
+            withdrawal_id = int(input('계좌ID: '))
+        except:
+            print('숫자만 입력하세요')
+        try:
+            withdrawal_amount = int(input('출금액: '))
+        except:
+            print('숫자만 입력하세요')
 
         if withdrawal_id in accounts.keys():
             BankAccount.withdrawal
         else:
             print('유효하지 않은 ID 입니다')
             break
+            # return
+            # sys.exit()
 
     elif i == 4:
         BankAccount.print_all_account()
